52428de6213713c3707b99c14cc3eb734096e933
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2019-02-09 17:39:58 +0100

    User application hotfixes

--- Application/server/users/urls.py
+++ Application/server/users/urls.py
@@ -6,10 +6,10 @@ from .api import views
 
 urlpatterns = [
     path('login/', obtain_auth_token),
-    path('', views.ListUserView.as_view()),
-    path('<pk>/', views.DetailUserView.as_view()),
     path('register/', views.CreateUserView.as_view()),
     path('update/<pk>/', views.UpdateUserView.as_view()),
     path('destroy/<pk>/', views.DestroyUserView.as_view()),
     path('current/', views.CurrentUserView.as_view()),
+    path('<pk>/', views.DetailUserView.as_view()),
+    path('', views.ListUserView.as_view()),
 ]
