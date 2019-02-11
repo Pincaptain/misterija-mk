2f1abec407b7b48fb644d3ae649b6c9ab9414d50
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2019-02-11 16:31:55 +0100

    Finalize profile model setup

--- Application/server/users/models.py
+++ Application/server/users/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from django.db import models
+from django.core.files.storage import default_storage
 from django.conf import settings
 
 from rest_framework.authtoken.models import Token
@@ -9,15 +10,23 @@ from rest_framework.authtoken.models import Token
 import os
 import uuid
 import random
+from shutil import copyfile
 
 def get_default_avatar_path():
-    path = settings.MEDIA_ROOT
-    count = len(os.listdir(os.path.join(path, 'users', 'avatars', 'defaults')))
+    media_path = settings.MEDIA_ROOT
+    defaults_path = os.path.join(media_path, 'users', 'avatars', 'defaults')
+    directory = os.listdir(defaults_path)
+    count = len(directory)
     index = random.randint(0, count-1)
     extension = 'png'
     filename = '{0}.{1}'.format(str(index), extension)
+    src = os.path.join(defaults_path, filename)
+    filename = '{0}.{1}'.format(uuid.uuid4(), extension)
+    dst = os.path.join(media_path, 'users', 'avatars', filename)
 
-    return os.path.join(path, 'users', 'avatars', 'defaults', filename)
+    copyfile(src, dst)
+
+    return os.path.join(media_path, 'users', 'avatars', filename)
 
 def get_avatar_path(instance, filename):
     extension = filename.split('.')[-1]
@@ -28,7 +37,7 @@ def get_avatar_path(instance, filename):
 class Profile(models.Model):
     bio = models.TextField(blank=True)
     location = models.CharField(max_length=60, blank=True)
-    avatar = models.ImageField(upload_to=get_avatar_path, blank=True)
+    avatar = models.ImageField(upload_to=get_avatar_path, default=get_default_avatar_path, blank=True, max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -48,3 +57,16 @@ def create_token(sender, **kwargs):
 def create_profile(sender, **kwargs):
     if kwargs['created']:
         Profile.objects.create(user=kwargs['instance'])
+
+@receiver(pre_save, sender=Profile)
+def delete_current_avatar(sender, **kwargs):
+    instance = kwargs['instance']
+
+    if instance.pk:
+        profile = Profile.objects.get(pk=instance.pk)
+        profile.avatar.delete(save=False)
+
+@receiver(post_delete, sender=Profile)
+def delete_avatar(sender, **kwargs):
+    instance = kwargs['instance']
+    instance.avatar.delete(save=False)
