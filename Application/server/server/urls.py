df78b2175240f51caeb129c30ba6d8494faa21dc
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2019-02-22 16:19:17 +0100

    Add posts rest application

--- Application/server/server/urls.py
+++ Application/server/server/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/users/', include('users.urls')),
+    path('api/posts/', include('posts.urls')),
 
     path('', include('react.urls')),
 ]
